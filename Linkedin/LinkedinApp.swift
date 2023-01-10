3ffb5efa67218108cc4aacb8dd80eab7365e23a7
Chirag Arora <arora2002chirag@gmail.com>
2023-01-10 23:13:46 +0530

    Initial Commit

---
+++ Linkedin/LinkedinApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedinApp.swift
+//  Linkedin
+//
+//  Created by chirag arora on 10/01/23.
+//
+
+import SwiftUI
+
+@main
+struct LinkedinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
